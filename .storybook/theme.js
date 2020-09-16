2100192f9cc340bcf9e4e9f487b53d2b815d3869
Kayne G.I <kayne.goncalves@healthbit.com.br>
2020-09-16 15:16:09 -0300

    Storybook settings

---
+++ .storybook/theme.js
@@ -0,0 +1,21 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+
+  colorSecondary: '#003a78',
+
+  appBg: '#003a78',
+  appContentBg: '#fafafa',
+  appBorderColor: '#003a78',
+
+  textColor: '#fff',
+
+  // Toolbar default and active colors
+  barTextColor: '#003a78',
+  barSelectedColor: '#003a78',
+  barBg: '#003a78',
+
+  brandTitle: 'Healthbit',
+  brandUrl: 'https://healthbit.com.br/',
+});
